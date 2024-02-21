9a3987136a4d4f79acf0a86087cfa03e7103a4e7
Nícolas Barcelos <nicolasbarcelos.almeida2@hotmail.com>
2024-02-21 16:49:36 -0300

    A

--- app.js
+++ app.js
@@ -54,15 +54,13 @@ const app = express()
     //mongoose
         mongoose.Promise = global.Promise;
 
-        const run = async () => {
+        mongoose
+            .connect(process.env.MONGO_URL).then(()=>{
+            console.log("Conectado Com Sucesso Ao Servidor MongoDB")
+        }).catch((err) => {
+            console.log("Falha ao Conectar ao MongoDB: " + err)
+        })
 
-            await connect(db.mongoURI).then(()=>{
-                console.log("Conectado Com Sucesso Ao Servidor MongoDB")
-            }).catch((err) => {
-                console.log("Falha ao Conectar ao MongoDB: " + err)
-            })
-
-        }
 
     //Public
     app.use(express.static(path.join(__dirname, "public")))
