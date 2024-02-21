d01e35e4a318c50671538d3d3cb5018bbc6ee4e8
Nícolas Barcelos <nicolasbarcelos.almeida2@hotmail.com>
2024-02-21 16:27:39 -0300

    a

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
-//Verificar quando estiver rodando no heroku ou no computador local
+//Verificar quando estiver rodando em produção ou no computador local
 
 if(process.env.NODE_ENV == "production"){
     module.exports = {mongoURI: "mongodb+srv://Admin:Admin123@blogapp.mcm2nhh.mongodb.net/?retryWrites=true&w=majority&appName=blogapp"}
